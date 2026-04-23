4151947cfc7618ce38f9baf46626d5ed34e5865c
Houssam <macroooowave@Houssams-MacBook-Air.local>
2026-04-23 15:17:18 +0100

    just for testing

--- src/main.cpp
+++ src/main.cpp
@@ -1,3 +1,17 @@
-#include "webserv.hpp"
+#include "../includes/HttpRequest.hpp"
+#include "../includes/HttpRespons.hpp"
 
-int main( void ) { std::cout << "this is just a test" << std::endl; }
+int main()
+{
+
+    Request R;
+
+    R.feed( "POST /upload HTTP/1.1\r\nHost: localhost\r\nContent-Length: "
+            "12\r\n\r\nHello World!",
+            82 );
+    try {
+        // R.feed();
+    } catch ( std::exception &e ) {
+        std::cerr << "Error " << e.what() << std::endl;
+    }
+}
